ed86bf82c3f7e88cfc4aaa93ea3da0f9af39f15a
Rafael <rafaelbrandao1992@gmail.com>
2023-06-01 17:52:43 -0300

    feat: change image to top

--- portifolio/src/components/MainCoverLetter.tsx
+++ portifolio/src/components/MainCoverLetter.tsx
@@ -4,6 +4,7 @@ export default class MainCoverLetter extends Component {
   render() {
     return (
       <div className="cover-letter-container">
+        <img src="/images/profile.png" alt="profile" />
         <div className="cover-title">
             <h1>
                 <span>Hi, I'm Rafael,</span> Fullstack Web Developer, from Brazil.
@@ -16,7 +17,6 @@ export default class MainCoverLetter extends Component {
             <button type="button" className="btn btn-gradient">Know more</button>
             <button type="button" className="btn btn-black">View my works</button>
         </div>
-        <img src="/images/profile.png" alt="profile" />
       </div>
     )
   }
