821291430a0989ad16562d1655a7fda36c7ddd2c
Rafael <rafaelbrandao1992@gmail.com>
2023-06-08 17:51:15 -0300

    refactor: change text from button download resume

--- frontend/src/components/ButtonResume.tsx
+++ frontend/src/components/ButtonResume.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const ButtonResume: React.FC = () => {
+  const { t } = useTranslation();
   const handleDownload = () => {
     window.open("/pdf/Rafael_Brandao_CV _May.pdf", '_blank');
   };
 
   return (
     <button onClick={handleDownload} className='btn btn-black'>
-      Download Resume
+      {t('button.download')}
     </button>
   );
 };
